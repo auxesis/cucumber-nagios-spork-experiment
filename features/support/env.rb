07b4a40beb67a71a74dac5002b844144924ef022
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-10-23 19:12:35 +0200

    sporkify cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,18 +1,28 @@
 #!/usr/bin/env ruby 
 
-require 'rubygems'                                                                                      
-require 'webrat'
-require 'webrat/mechanize'
 
-class ResponseHelper
-  def response
-    webrat_session.response
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+
+  require File.join(File.dirname(__FILE__), '..', '..', 'vendor', 'gems', 'environment')
+  require 'cucumber'  
+  require 'webrat'
+  require 'webrat/mechanize'
+  
+  class ResponseHelper
+    def response
+      webrat_session.response
+    end
+  end
+  
+  World do
+    ResponseHelper.new
+    Webrat::Session.new(Webrat::MechanizeAdapter.new)
   end
 end
 
-World do
-  ResponseHelper.new
-  Webrat::Session.new(Webrat::MechanizeAdapter.new)
-end
+
 
 
